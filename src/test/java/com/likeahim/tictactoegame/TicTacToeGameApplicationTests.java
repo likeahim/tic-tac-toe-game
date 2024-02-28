8df2f929a4b943fe339801d735ce15a1258f94cc
likeahim <michal.panko21@gmail.com>
2024-02-28 15:22:26 +0100

    first commit

---
+++ src/test/java/com/likeahim/tictactoegame/TicTacToeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likeahim.tictactoegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
